c82fea794d6620f2fc6c8ad70555f2d8ae3bc145
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2022-12-15 16:25:09 +0300

    update

--- newconvertor/src/App.js
+++ newconvertor/src/App.js
@@ -6,24 +6,69 @@ const BASE_URL = 'https://open.er-api.com/v6/latest/USD'
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
-  console.log(currencyOptions)
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
       })
   }, [])
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null){
+      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+      .then(res => res.json())
+      .then(data => setExchangeRate(data.rates[toCurrency]))
+    }
+    
+  }, [fromCurrency, toCurrency])
+
+  function handleFromAmountChange(Event) {
+    setAmount(Event.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(Event) {
+    setAmount(Event.target.value)
+    setAmountInFromCurrency(false)
+  }
+
   return (
     <>
       <h1>Convert</h1>
       <CurrencyRow 
         currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={Event => setFromCurrency(Event.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
       />
       <div className='equals'>=</div>
       <CurrencyRow 
         currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={Event => setToCurrency(Event.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
       />
     </>
    );
--- newconvertor/src/CurrencyRow.js
+++ newconvertor/src/CurrencyRow.js
@@ -2,14 +2,18 @@ import React from "react";
 
 export default function CurrencyRow(props) {
     const {
-        currencyOptions
+        currencyOptions,
+        selectedCurrency,
+        onChangeCurrency,
+        onChangeAmount,
+        amount
     } = props
     return (
         <div>
-            <input type="number" className="input" />
-            <select>
+            <input type="number" className="input" value={amount} onChange={onChangeAmount}/>
+            <select value={selectedCurrency} onChange={onChangeCurrency}>
                 {currencyOptions.map(option => (
-                <option value={option}>{option}</option>
+                <option key={option} value={option}>{option}</option>
                 ))}
             </select>
         </div>
